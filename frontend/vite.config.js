4e91ed1a5b717c52adf0bf5474c36c36b8735fcc
devendra <devendra@sourceved.com>
2026-03-17 16:39:05 +0530

    version updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   build: {
     outDir: `../${path.basename(path.resolve('..'))}/public/frontend`,
     emptyOutDir: true,
-    target: 'es2019',
+    target: 'es2015',
   },
   optimizeDeps: {
     include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
